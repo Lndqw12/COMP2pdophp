4f6869f6dabf4db398acfe53ad691ea78afc4bd9
Lndqw12 <COMP222012016@mail.ru>
2016-03-31 17:56:21 +0300

    copm2
    copm2

---
+++ PDO.php
@@ -0,0 +1,22 @@
+﻿<meta charset ="utf-8">
+<?php
+$host='localhost';
+$dbname='nazvanie';
+$user='redactor';
+$password='12345';
+try {
+$db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+echo $e->getMessage();
+}
+$name = 'Ксения';
+//$stmt = $pdo->prepare('SELECT name FROM nazvanie1 WHERE name = ?');
+//$stmt = $pdo->prepare('SELECT name FROM nazvanie1');
+$stmt->execute(array('name' => $name));
+foreach ($stmt as $row)
+{
+    echo $row['name'] . "\n";
+}
+?>
